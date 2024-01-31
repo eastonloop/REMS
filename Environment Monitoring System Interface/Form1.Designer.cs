da4c00da768d6c24448111a3d7a976000b5f8174
wsqua <wsqua@DESKTOP-SVTER53>
2024-01-31 10:49:44 -0600

    Add project files.

---
+++ Environment Monitoring System Interface/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Environment_Monitoring_System_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(348, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(284, 259);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Environment Monitoring System Interface/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using System.Reflection.Emit;
+using OfficeOpenXml;
+using DocumentFormat.OpenXml.Vml;
+
+namespace Environment_Monitoring_System_Interface
+{
+    public partial class Form1 : Form
+    {
+        string selectedCOMPort;
+        private ManagementEventWatcher comPortWatcher;
+         
+        bool ArduinoConnected;
+        public bool isConnectedToCU = false;
+        string data;
+
+        private async Task<string> ReadResponse()
+        {
+            using (var reader = new StreamReader(serialPort1.BaseStream))
+            {
+                try
+                {
+                    string response = await reader.ReadLineAsync();
+                    return response;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error");
+                    return string.Empty;
+                }
+            }
+        }
+        private async void autoConnectCOMPort()
+        {
+            isConnectedToCU = false;
+            string[] comPorts = GetCOMPorts();
+
+            foreach (string port in comPorts)
+            {
+                if (serialPort1.IsOpen)
+                {
+                    return;
+                }
+
+                serialPort1.PortName = port;
+                serialPort1.BaudRate = 9600;
+                serialPort1.DtrEnable = true;
+                serialPort1.DataBits = 8;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.Encoding = System.Text.Encoding.Default;
+
+                try
+                {
+                    await Task.Run(() => serialPort1.Open());
+                    serialPort1.Write("Z");
+                    var timeoutTask = Task.Delay(TimeSpan.FromSeconds(1));
+                    var responseTask = ReadResponse();
+
+                    await Task.WhenAny(responseTask, timeoutTask);
+
+                    if (responseTask.IsCompleted && responseTask.Result == "Connected")
+                    {
+                        serialPort1.Open();
+                        isConnectedToCU = true;
+                        textBox1.Text = "Connected";
+                        serialPort1.DataReceived += SerialPort1_DataReceived;
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception during COM port communication: {ex.Message}");
+                    textBox1.Text = "Not connected";
+                }
+                finally
+                {
+                    if (!isConnectedToCU)
+                    {
+                        serialPort1.Close();
+                    }
+                }
+            }
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            data = serialPort1.ReadLine();
+
+            textBox2.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text = data;
+            });
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comPortWatcher = new ManagementEventWatcher();
+            WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2 OR EventType = 3");
+            comPortWatcher.EventArrived += new EventArrivedEventHandler(ComPortEventArrived);
+            comPortWatcher.Query = query;
+            comPortWatcher.Start();
+            autoConnectCOMPort();
+            GetData();
+        }
+       
+        private void ComPortEventArrived(object sender, EventArrivedEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(autoConnectCOMPort));
+            }
+            else
+            {
+                autoConnectCOMPort();
+            }
+        }
+
+        private string[] GetCOMPorts()
+        {
+            string[] availablePorts = SerialPort.GetPortNames();
+            return availablePorts;
+        }
+
+        private void populateCOMPorts()
+        {
+            string[] availablePorts = SerialPort.GetPortNames();
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            populateCOMPorts();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            comPortWatcher.Stop();
+            comPortWatcher.Dispose();
+        }
+
+        private async void GetData()
+        {
+            if (serialPort1.IsOpen && isConnectedToCU)
+            {
+                var responseTask = ReadResponse();
+                await Task.WhenAny(responseTask);
+
+                if (responseTask.IsCompleted)
+                {
+                    return;
+                }
+            }
+           
+        }
+    }
+}
